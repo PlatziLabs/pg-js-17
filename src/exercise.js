cdd0f9e88152a4ff43ee460681b9e490574b2266
Nicolas Molina Monroy <hi@nicobytes.com>
2022-10-08 15:19:34 -0500

    save

--- src/exercise.js
+++ src/exercise.js
@@ -1,3 +1,20 @@
-export function pascalTriangle(number) {
-  // Tu código aquí 👈
+export function pascalTriangle(lineNumber) {
+  const matriz = [
+    [1],
+    [1, 1]
+  ];
+
+  if (lineNumber < 2) {
+    return matriz[lineNumber]
+  } else {
+    for (let i = 2; i <= lineNumber; i++) {
+      matriz.push([1])
+      for (let j = 1; j < i; j++) {
+        matriz[i].push(matriz[i - 1][j - 1] + matriz[i - 1][j])
+      }
+      matriz[i].push(1)
+    }
+    return matriz[lineNumber]
+  }
+
 }
